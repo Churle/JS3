c81bc0b708492a163dfba244f35439956888ec0c
ChurMaster <charlie_cwv@hotmail.com>
2024-01-23 03:41:16 -0300

    Adding test1

---
+++ test1/test1.js
@@ -0,0 +1,4 @@
+let palavrapessoa =prompt('Digite o numero de pessoa(s)');
+console.log(palavrapessoa);
+let quantidade = palavrapessoa ? 'palavra' : 'palavras';
+alert(`${palavrapessoa} ${quantidade}`);
